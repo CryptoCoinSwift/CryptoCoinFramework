27d45295536256fb218aee8381916a917592bb20
Sjors Provoost <sjors@sprovoost.nl>
2014-06-23 18:24:49 +0200

    XCode configuration stuff. Reflect updated CoinKey submodule.

--- Crypto Coin Swift.h
+++ Crypto Coin Swift.h
@@ -15,4 +15,6 @@ FOUNDATION_EXPORT const unsigned char Crypto_Coin_SwiftVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <Crypto_Coin_Swift/PublicHeader.h>
 
+#import <Crypto_Coin_Swift/CoinKey.h>
+
 
