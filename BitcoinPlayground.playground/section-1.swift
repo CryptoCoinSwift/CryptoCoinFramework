e4a3d3c9243b4b715e0378e4fb7664efa79542ae
Sjors Provoost <sjors@sprovoost.nl>
2014-06-23 21:53:12 +0200

    Moved some things around. Still can't get playground to work.

--- BitcoinPlayground.playground/section-1.swift
+++ BitcoinPlayground.playground/section-1.swift
@@ -1,15 +1,23 @@
 import UIKit
 
+let c = 4
+
+
 import CryptoCoin
+
+let b = 2
+let a = 5
+
+
 // Unfortunately this doesn't seem to be possible yet.
 // http://stackoverflow.com/questions/24045245/how-to-import-own-classes-from-your-own-project-into-a-playground
 // http://stackoverflow.com/questions/24046160/how-to-i-import-3rd-party-frameworks-into-xcode-playground
 // I tried creating a symlink as suggested in the second post, but it still won't find the framework:
 // ln -s ~/DemoApp/Crypto\ Coin\ Swift /Applications/Xcode6-Beta.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator8.0.sdk/System/Library/Frameworks/CryptoCoin.framework
-// Here DemoApp is an example app and CryptoCoin is a submodule at "Crypto Coin Swift". 
+// I also tried copying rather than symlinking.
+// Here DemoApp is an example app and CryptoCoin is a submodule at "Crypto Coin Swift".
 
 let coinKey = CoinKey.generateRandom()
 
+
 coinKey.publicAddress
-
-
--- Headers/CryptoCoin.h
+++ Headers/CryptoCoin.h
@@ -1,6 +1,6 @@
 //
-//  Crypto Coin Swift.h
-//  Crypto Coin Swift
+//  CryptoCoin.h
+//  Crypto Coin
 //
 //  Created by Sjors Provoost on 23-06-14.
 //
@@ -13,8 +13,6 @@ FOUNDATION_EXPORT double CryptoCoinVersionNumber;
 //! Project version string for Crypto Coin Swift.
 FOUNDATION_EXPORT const unsigned char CryptoCoinVersionString[];
 
-// In this header, you should import all the public headers of your framework using statements like #import <Crypto_Coin_Swift/PublicHeader.h>
-
-#import <CryptoCoin/CoinKey.h>
+#import <CryptoCoin/CoinKey-Swift.h>
 
 
