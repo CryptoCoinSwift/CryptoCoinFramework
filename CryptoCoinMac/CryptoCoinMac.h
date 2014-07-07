080124ebfb16e8ccf5abf5542c888f328964c90d
Sjors Provoost <sjors@sprovoost.nl>
2014-07-07 12:16:41 +0200

    Add Mac Framework.

---
+++ CryptoCoinMac/CryptoCoinMac.h
@@ -0,0 +1,19 @@
+//
+//  CryptoCoinMac.h
+//  CryptoCoinMac
+//
+//  Created by Sjors Provoost on 07-07-14.
+//  Copyright (c) 2014 Crypto Coin Swift. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for CryptoCoinMac.
+FOUNDATION_EXPORT double CryptoCoinMacVersionNumber;
+
+//! Project version string for CryptoCoinMac.
+FOUNDATION_EXPORT const unsigned char CryptoCoinMacVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CryptoCoinMac/PublicHeader.h>
+
+
