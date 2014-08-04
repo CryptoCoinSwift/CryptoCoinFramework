ec502ca3ca872cac1d846ca1e5bc7889a9af35ed
Sjors Provoost <sjors@sprovoost.nl>
2014-08-05 00:17:58 +0200

    Beta 5 - Much slower...
    Address generation on iPhone 5S takes 5 seconds (was 0.6). iPad mini is
    down from 4 seconds to 58 seconds.

---
+++ Bitcoin.playground/section-1.swift
@@ -0,0 +1,14 @@
+// Playground - noun: a place where people can play
+
+import Cocoa
+import CryptoCoin
+
+1
+
+let bitcoin = CoinKey()
+
+// Address:
+bitcoin.publicAddress
+
+// Private key
+bitcoin.wif
